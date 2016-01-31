01dc14fd1c36a20616c9d9cbba29fc2d77ad5b1a
yves-chan <chanyves@gmail.com>
2016-01-31 09:54:53 -0800

    fixed scoreboard

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ fi
 grunt build;
 cp -rf dist/ ../deployCandyDance/dist/
 ls -la ../deployCandyDance/dist/
-git --git-dir=../deployCandyDance/.git add .
+git --git-dir=../deployCandyDance/.git add $PWD
 git --git-dir=../deployCandyDance/.git commit -m "$1"
 git --git-dir=../deployCandyDance/.git push heroku master
